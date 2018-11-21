cc662afa21729c90581e7cab5ba24554b7d00328
Angel Kyriako <angelkyriako@gmail.com>
2018-11-22 00:54:21 +0200

    initial commit: upgraded version of angelkyriako/FCC Advanced Node.js challanges

---
+++ README.md
@@ -0,0 +1,7 @@
+Pixelator
+============================
+
+Expansion upon the final step of the [Advanced node.js/express challenges repo](https://github.com/AngelKyriako/FccAdvancedNodejsExpressApp) 
+
+
+\ ゜o゜)ノ
---
+++ config.js
@@ -0,0 +1,50 @@
+var os = require("os");
+
+var self = {
+
+  environment: process.env.NODE_ENV || 'development',
+
+  hostname: os.hostname(),
+
+  port: process.env.PORT || 3000,
+
+  ssl: process.env.SSL != 'false',
+  
+  auth: {
+
+    local: {
+      saltRounds: 10
+    },
+
+    // not currently used
+    //
+    // difficult to be unit tested & setup ...to be continued.
+    //
+    // facebook: {
+    //   clientID: process.env.FACEBOOK_CLIENT_ID || '1078155265586289',
+    //   clientSecret: process.env.FACEBOOK_SECRET || 'e96b7e79de093bd3e52566af32d93029',
+    //   scopes: ['public_profile'],
+    //   profileFields: ['id', 'displayName', 'photos']
+    // },
+
+    // google: {
+    //   clientID: process.env.GOOGLE_CLIENT_ID || '241088503074-111odkquc7eoud4l6nnbh1b7v7afah9t.apps.googleusercontent.com',
+    //   clientSecret: process.env.GOOGLE_SECRET || 'Ufs-cEDnX105gkuUbqfqbN7n',
+    //   scopes: [],
+    //   profileFields: []
+    // }
+  },
+
+  db: {
+    mongoUrl: process.env.MONGO_URL || 'mongodb://fccuser:fccuser123@ds033976.mlab.com:33976/fcc-node-advanced-challenges'
+  },
+
+  session: {
+    secret: process.env.SESSION_SECRET || 'IamABadBadSessionSecret'
+  }
+};
+
+self.serverUri = (self.ssl ? 'https://' : 'http://') + self.hostname + ':' + self.port;
+
+
+module.exports = self;
